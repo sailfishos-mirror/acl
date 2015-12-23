0b8d28449925711e69d228c24f327c5bf0c2b627
Mike Frysinger <vapier@gentoo.org>
2015-12-23 13:02:03 -0500

    use portable AC_C_BIGENDIAN
    This allows us to avoid using endian.h.

--- libacl/byteorder.h
+++ libacl/byteorder.h
@@ -15,9 +15,9 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <endian.h>
+#include "config.h"
 
-#if __BYTE_ORDER == __BIG_ENDIAN
+#ifdef WORDS_BIGENDIAN
 # define cpu_to_le16(w16) le16_to_cpu(w16)
 # define le16_to_cpu(w16) ((u_int16_t)((u_int16_t)(w16) >> 8) | \
                            (u_int16_t)((u_int16_t)(w16) << 8))
@@ -26,12 +26,10 @@
                            (u_int32_t)(((u_int32_t)(w32) >> 8) & 0xFF00) | \
                            (u_int32_t)(((u_int32_t)(w32) << 8) & 0xFF0000) | \
 			   (u_int32_t)( (u_int32_t)(w32) <<24))
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#else
 # define cpu_to_le16(w16) ((u_int16_t)(w16))
 # define le16_to_cpu(w16) ((u_int16_t)(w16))
 # define cpu_to_le32(w32) ((u_int32_t)(w32))
 # define le32_to_cpu(w32) ((u_int32_t)(w32))
-#else
-# error unknown endianess?
 #endif
 
