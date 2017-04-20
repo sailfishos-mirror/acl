sailfishos-mirror/acl

commit 5dac69ce914202c587f00da638123403111ca673

libacl: Ignore warning in parse_acl_entry()
In this function, we call int2ext() on a temporary object on the stack.
When int2ext() checks for a NULL pointer, gcc's -Waddress warning points
out that the address of the temporary object can never be NULL, which is
correct but not helpful:

  warning: the address of 'entry_obj' will always evaluate as 'true' [-Waddress]

Suppress this warning.

libacl/acl_from_text.c
@@ -304,7 +304,10 @@ parse_acl_entry(const char **text_p, acl_t *acl_p)
 create_entry:
 	if (acl_create_entry(acl_p, &entry_d) != 0)
 		return -1;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress"
 	if (acl_copy_entry(entry_d, int2ext(&entry_obj)) != 0)
+#pragma GCC diagnostic pop
 		return -1;
 	return 0;
 

commit fa5f6835e00c4927cae051328642b2a5a0d8b1c1

libacl: Fix acl_from_text() returning NULL on all input
Commit 5dac69ce914202c587f00da638123403111ca673 introduced a regression:
apparently, gcc treats
  #pragma GCC diagnostic pop
as a kind of statement, so this causes parse_acl_entry()
to return -1 unconditionally, and therefore causes acl_from_text()
to return NULL unconditionally, too.

Fix this by moving the pragma after the "if" statement.

Reported-by: Valery Inozemtsev <shrek@altlinux.org>

libacl/acl_from_text.c
@@ -307,8 +307,8 @@ create_entry:
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Waddress"
 	if (acl_copy_entry(entry_d, int2ext(&entry_obj)) != 0)
-#pragma GCC diagnostic pop
 		return -1;
+#pragma GCC diagnostic pop
 	return 0;
 
 fail: