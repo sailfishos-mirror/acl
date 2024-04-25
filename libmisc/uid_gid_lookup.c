sailfishos-mirror/acl

commit 3737f000d3f17cd283f51eeacac21a71a3472053

use thread-safe getpwnam_r and getgrnam_r
- Switch from `getpwnam` and `getgrnam` to `getpwnam_r` and `getgrnam_r`
for thread safety.
- Extract duplicated `get_id`, `get_uid`, and `get_gid` to common module
`libmisc/get.c` from `libacl/acl_from_text.c` and `tools/parse.c`.
- Update tests for buffer resizing due to these changes.

This ensures thread safety without changing the behavior.

(Minor additional cleanups applied while merging.)

libmisc/uid_gid_lookup.c
@@ -1,0 +1,127 @@
+/*
+  File: uid_gid_lookup.c
+
+  Copyright (C) 2023 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2.1 of the License, or (at
+  your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "config.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+#include <unistd.h>
+#include "libacl.h"
+#include "misc.h"
+
+int
+get_id(const char *token, id_t *id_p)
+{
+	char *ep;
+	long l;
+	l = strtol(token, &ep, 0);
+	if (*ep != '\0')
+		return -1;
+	if (l < 0) {
+		/*
+		  Negative values are interpreted as 16-bit numbers
+		  so that id -2 maps to 65534 (nobody/nogroup), etc.
+		*/
+		l &= 0xFFFF;
+	}
+	*id_p = l;
+	return 0;
+}
+
+static char *
+grow_buffer(char **buffer, size_t *bufsize, int type)
+{
+	long size = *bufsize;
+	char *buf;
+
+	if (!size) {
+		size = sysconf(type);
+		if (size <= 0)
+			size = 16384;
+	} else {
+		size *= 2;
+	}
+
+	buf = realloc(*buffer, size);
+	if (buf) {
+		*buffer = buf;
+		*bufsize = size;
+	}
+	return buf;
+}
+
+int
+get_uid(const char *token, uid_t *uid_p)
+{
+	struct passwd passwd, *result = NULL;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	int err;
+	if (get_id(token, uid_p) == 0)
+		return 0;
+
+	for(;;) {
+		if(!grow_buffer(&buffer, &bufsize, _SC_GETPW_R_SIZE_MAX))
+			break;
+
+		err = getpwnam_r(token, &passwd, buffer, bufsize, &result);
+		if (result) {
+			*uid_p = passwd.pw_uid;
+			break;
+		}
+		if (err == ERANGE)
+			continue;
+		errno = err ? err : EINVAL;
+	}
+	free(buffer);
+	return result ? 0 : -1;
+}
+
+
+int
+get_gid(const char *token, gid_t *gid_p)
+{
+	struct group group, *result = NULL;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	int err;
+	if (get_id(token, (uid_t *)gid_p) == 0)
+		return 0;
+
+	for(;;) {
+		if(!grow_buffer(&buffer, &bufsize, _SC_GETGR_R_SIZE_MAX))
+			break;
+
+		err = getgrnam_r(token, &group, buffer, bufsize, &result);
+		if (result) {
+			*gid_p = group.gr_gid;
+			break;
+		}
+		if (err == ERANGE)
+			continue;
+		errno = err ? err : EINVAL;
+		break;
+	}
+
+	free(buffer);
+	return result ? 0 : -1;
+}

include/misc.h
@@ -19,6 +19,7 @@
 #define __MISC_H
 
 #include <stdio.h>
+#include <sys/types.h>
 
 /* Mark library internal functions as hidden */
 #if defined(HAVE_VISIBILITY_ATTRIBUTE)
@@ -33,6 +34,10 @@ hidden const char *__acl_quote(const char *str, const char *quote_chars);
 hidden char *__acl_unquote(char *str);
 
 hidden char *__acl_next_line(FILE *file);
+
+hidden int get_id(const char *token, id_t *id_p);
+hidden int get_uid(const char *token, uid_t *uid_p);
+hidden int get_gid(const char *token, gid_t *gid_p);
 
 #ifdef ENABLE_NLS
 # include <libintl.h>

libacl/acl_from_text.c
@@ -126,64 +126,6 @@ after_token:
 }
 
 
-static int
-get_id(const char *token, id_t *id_p)
-{
-	char *ep;
-	long l;
-	l = strtol(token, &ep, 0);
-	if (*ep != '\0')
-		return -1;
-	if (l < 0) {
-		/*
-		  Negative values are interpreted as 16-bit numbers,
-		  so that id -2 maps to 65534 (nobody/nogroup), etc.
-		*/
-		l &= 0xFFFF;
-	}
-	*id_p = l;
-	return 0;
-}
-
-
-static int
-get_uid(const char *token, uid_t *uid_p)
-{
-	struct passwd *passwd;
-
-	if (get_id(token, uid_p) == 0)
-		return 0;
-	errno = 0;
-	passwd = getpwnam(token);
-	if (passwd) {
-		*uid_p = passwd->pw_uid;
-		return 0;
-	}
-	if (errno == 0)
-		errno = EINVAL;
-	return -1;
-}
-
-
-static int
-get_gid(const char *token, gid_t *gid_p)
-{
-	struct group *group;
-
-	if (get_id(token, (uid_t *)gid_p) == 0)
-		return 0;
-	errno = 0;
-	group = getgrnam(token);
-	if (group) {
-		*gid_p = group->gr_gid;
-		return 0;
-	}
-	if (errno == 0)
-		errno = EINVAL;
-	return -1;
-}
-
-
 /*
 	Parses the next acl entry in text_p.
 

libmisc/Makemodule.am
@@ -1,6 +1,7 @@
 noinst_LTLIBRARIES += libmisc.la
 
 libmisc_la_SOURCES = \
+	libmisc/uid_gid_lookup.c \
 	libmisc/high_water_alloc.c \
 	libmisc/next_line.c \
 	libmisc/quote.c \

test/test_group.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include <assert.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
@@ -10,7 +11,6 @@
 
 #define TEST_GROUP "test/test.group"
 static char grfile[] = BASEDIR "/" TEST_GROUP;
-
 
 #define ALIGN_MASK(x, mask)    (((x) + (mask)) & ~(mask))
 #define ALIGN(x, a)            ALIGN_MASK(x, (typeof(x))(a) - 1)
@@ -116,6 +116,16 @@ EXPORT
 int getgrnam_r(const char *name, struct group *grp, char *buf, size_t buflen,
 	       struct group **result)
 {
+	static size_t last_buflen = -1;
+
+	assert(last_buflen == -1 || buflen > last_buflen);
+	if (buflen < 170000) {
+		last_buflen = buflen;
+		*result = NULL;
+		return ERANGE;
+	}
+	last_buflen = -1;
+
 	return test_getgr_match(grp, buf, buflen, result, match_name, name);
 }
 

test/test_passwd.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include <assert.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
@@ -110,6 +111,16 @@ EXPORT
 int getpwnam_r(const char *name, struct passwd *pwd, char *buf, size_t buflen,
 	       struct passwd **result)
 {
+	static size_t last_buflen = -1;
+
+	assert(last_buflen == -1 || buflen > last_buflen);
+	if (buflen < 170000) {
+		last_buflen = buflen;
+		*result = NULL;
+		return ERANGE;
+	}
+	last_buflen =- 1;
+
 	return test_getpw_match(pwd, buf, buflen, result, match_name, name);
 }
 

tools/parse.c
@@ -111,70 +111,6 @@ after_token:
 }
 
 
-static int
-get_id(
-	const char *token,
-	id_t *id_p)
-{
-	char *ep;
-	long l;
-	l = strtol(token, &ep, 0);
-	if (*ep != '\0')
-		return -1;
-	if (l < 0) {
-		/*
-		  Negative values are interpreted as 16-bit numbers,
-		  so that id -2 maps to 65534 (nobody/nogroup), etc.
-		*/
-		l &= 0xFFFF;
-	}
-	*id_p = l;
-	return 0;
-}
-
-
-static int
-get_uid(
-	const char *token,
-	uid_t *uid_p)
-{
-	struct passwd *passwd;
-
-	if (get_id(token, (id_t *)uid_p) == 0)
-		goto accept;
-	passwd = getpwnam(token);
-	if (passwd) {
-		*uid_p = passwd->pw_uid;
-		goto accept;
-	}
-	return -1;
-
-accept:
-	return 0;
-}
-
-
-static int
-get_gid(
-	const char *token,
-	gid_t *gid_p)
-{
-	struct group *group;
-
-	if (get_id(token, (id_t *)gid_p) == 0)
-		goto accept;
-	group = getgrnam(token);
-	if (group) {
-		*gid_p = group->gr_gid;
-		goto accept;
-	}
-	return -1;
-
-accept:
-	return 0;
-}
-
-
 /*
 	Parses the next acl entry in text_p.
 

commit 56abe432b65801f31277fb9a3bca0f9e31502315

libmisc: __acl_get_uid(): fix memory wasting loop if user does not exist
I noticed that `acl_from_text()` unexpectedly returns ENOMEM for invalid
user names. The reason for this is a missing break statement in the for
loop in `__acl_get_uid()`, which causes the loop to act as if ERANGE was
returned from `getpwnam_r()`, thereby exponentially increasing the
buffer size to (in my case) multiple gigabytes, until `grow_buffer()`
reports ENOMEM, which terminates the `__acl_get_uid()` function.

This is a pretty costly "no such user" lookup that can disturb a
process's heap memory management, but can also cause a process to fail
e.g. if it is multithreaded and other threads encounter an ENOMEM,
before `__acl_get_uid()` frees the gigantic heap buffer and returns.
The allocated memory isn't actually used. Therefore on Linux it should
not affect other processes by default, due to its overcommit memory
and lazy memory allocation strategy.

Fix this by properly terminating the for loop on any conditions except
an ERANGE error being reported. The same break statement correctly
exists in `__acl_get_gid()` already.

Fixes: 3737f00 ("use thread-safe getpwnam_r and getgrnam_r")
Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

libmisc/uid_gid_lookup.c
@@ -91,6 +91,7 @@ __acl_get_uid(const char *token, uid_t *uid_p)
 		if (err == ERANGE)
 			continue;
 		errno = err ? err : EINVAL;
+		break;
 	}
 	free(buffer);
 	return result ? 0 : -1;